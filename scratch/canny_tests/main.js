fe9b8b1b321a558e0d92bba3c79a2e82217b9dc9
Doug Rosman <drosman@saic.edu>
2021-05-05 18:21:53 -0500

    add canny tests

---
+++ scratch/canny_tests/main.js
@@ -0,0 +1,18 @@
+const NUM_IMAGES = 50;
+let HEADERS = ['source','25-150', '25-200', '25-300', '50-150', '50-200', '50-300', '100-150', '100-200', '100-300'];
+const NUM_TESTS = HEADERS.length-1;
+let entry = ``;
+
+
+for(let i = 0; i < NUM_TESTS; i++) {
+    let newColumn = `<div class="column">
+                        <h3 class="column__header">${HEADERS[i]}</h3>
+                     </div>`
+    $('.container').append(newColumn);
+    for(let j = 0; j < NUM_IMAGES; j++) {
+        let counter = (100 + j + 1) + '';
+        let counterString = counter.slice(1);
+        let tempEntry = `<img src="images/${HEADERS[i]}/better-hands_00${counterString}.PNG" alt="canny-hand" class="sample">`
+        $('.column:last-child').append(tempEntry);
+    }
+}
